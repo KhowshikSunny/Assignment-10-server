2187500b1f577c87d1b405dba48a902809f43d5d
KhowshikSunny <khowshik.sunny7@gmail.com>
2026-01-20 02:47:57 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// IMegHHjcASAme0Ja
+// book-heaven
+
+const uri =
+  "mongodb+srv://book-heaven:IMegHHjcASAme0Ja@cluster0.xuoqyn9.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("bookDB");
+    const bookCollection = db.collection("bookHaven");
+
+    app.get("/all-books", async (req, res) => {
+      const result = await bookCollection.find().toArray();
+      console.log("result:", result);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
